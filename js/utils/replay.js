2474931a22e6d5fbbd7d01b6dd441c312a43fa70
Mikhail Sorokolet <mikhail40let@gmail.com>
2017-10-26 12:50:16 +0300

    правки под тревиса

--- js/utils/replay.js
+++ js/utils/replay.js
@@ -2,14 +2,10 @@ import {showScreen} from './show-element.js';
 import welcome from '../screens/welcome/welcome.js';
 import {initialState, gameData} from '../data.js';
 import header from '../screens/header/header.js';
-import artist from '../screens/artist/artist.js';
-import genre from '../screens/genre/genre.js';
 
 export default function mainReplayHandler() {
   initialState.notes = 3;
   gameData.stat = [];
-  // artist._element = null;
-  // genre._element = null;
   header._element = null;
   header.stopTimer();
   showScreen(welcome);
